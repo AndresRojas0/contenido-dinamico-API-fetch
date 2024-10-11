d4f50a1321243d512a7712c65ca15d04b38e1c8c
unknown <andresgustavorojas0@gmail.com>
2024-10-11 11:33:27 -0300

    handling the fetched data

--- js/api-fetch-sincro.js
+++ js/api-fetch-sincro.js
@@ -1,8 +1,37 @@
 function cargarDatos(url) {
     let dinamico = document.querySelector('#dinamico');
+    
     fetch(url)
-        .then(respuesta => respuesta.text())
-        .then(datos => dinamico.innerHTML = datos);
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            let lista = document.createElement('ul');
+            
+            datos.forEach(d => {
+                let item = document.createElement('li');
+                let texto = `
+                    <hr><br>
+                    ID: ${d.id} -
+                    Name: ${d.name} -
+                    Username: ${d.username} -
+                    Email: ${d.email}<br>
+                    Address: ${d.address.street}, ${d.address.suite}, ${d.address.city}, ${d.address.zipcode}<br>
+                    Geo: Lat ${d.address.geo.lat}, Lng ${d.address.geo.lng}<br>
+                    Phone: ${d.phone} - 
+                    Website: ${d.website}<br>
+                    Company: ${d.company.name} - ${d.company.catchPhrase} - ${d.company.bs}<br>
+                    &#160<br>
+                `;
+                item.innerHTML = texto;
+                lista.appendChild(item);
+            });
+            
+            dinamico.innerHTML = ''; // Clear any existing content
+            dinamico.appendChild(lista);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            dinamico.innerHTML = 'Error loading data';
+        });
 }
 
 async function cargarPagina(pagina, datosDinamicos) {
